63af774ff57faf561fc31efe09b63afbf8bd1794
MakerSquare Student <students@themakersquare.com>
2013-09-05 11:01:43 -0500

    added first class file

---
+++ class.rb
@@ -0,0 +1,12 @@
+5.send(:*, 5)
+"omg".send(:upcase)
+['a', 'b', 'c'].send(:at, 1)
+['a', 'b', 'c'].send(:insert, 2, 'o', 'h', 'n', 'o')
+{}.send(:size)
+{character: "Mario"}.send(:has_key?, :characters)
+
+6 - 32
+{html: true, json: false}.keys
+"MakerSquare" * 6
+"MakerSquare".split('a')
+[1,2,3,4,5][3]
